6cad289f3befff1a74632f18b4d6027c6277c5ef
Koti Reddy <kotireddyk0485@gmail.com>
2024-09-13 20:36:36 +0530

    added basic setup for react js project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
+import HelloWorld from './HelloWorld';
 
-function App(){
-    return <h1>
-        Hello World
-    </h1>
+function App() {
+	return (
+		<div>
+			<HelloWorld />
+		</div>
+	);
 }
 
 export default App;
